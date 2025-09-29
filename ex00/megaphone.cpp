e087b66796ae4b84e7e61f9e6561059b06c8e5a2
doglaswicht <doglasbastos@hotmail.com>
2025-09-29 16:27:41 +0200

    correction Bug ENV

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -13,22 +13,27 @@
 #include <iostream>
 #include <cctype>
 
+
 int main(int argc, char **argv)
 {
     int i = 1;
     int j = 0;
 
-    if(argc == 1){std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl; return 0;}
+    if(argc == 1){std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl; 
+        return 0;}
     
     while(i < argc)
     {
-        while(argv[i][j])
+        j = 0;
+        while(argv[i][j] != '\0')
         {
             std::cout << (char) std::toupper(argv[i][j]);
             j++;
         }
-        j = 0;
+        if(i != argc - 1)
+            std::cout << " ";
         i++;
     }
+    std::cout << std::endl;
      return 0;
 }
