c17e0c54a7765f1726053de93b7273ce1917657d
dw <doglasbastos@hotmail.com>
2025-10-09 12:15:48 +0200

    ex01fix string

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: dleite-b <dleite-b@student.42lausanne.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/01 03:03:28 by dleite-b          #+#    #+#             */
-/*   Updated: 2025/10/09 12:09:19 by dleite-b         ###   ########.fr       */
+/*   Updated: 2025/10/09 12:13:45 by dleite-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ int main()
     
     while(true)
     {
-        std::cout << "Enter command (ADD, SEARCH, EXIT)";
+        std::cout << "Enter command (ADD, SEARCH, EXIT): ";
         std::getline(std::cin, command);
         
         if(command == "ADD")
