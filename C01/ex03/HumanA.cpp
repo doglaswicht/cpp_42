3fa27d9cf2bde4f578c7ca39c1a49a92480c0684
doglaswicht <doglasbastos@hotmail.com>
2026-02-11 10:55:27 +0100

    ex03 done

---
+++ C01/ex03/HumanA.cpp
@@ -0,0 +1,10 @@
+
+
+#include "HumanA.hpp"
+#include <iostream>
+
+HumanA::HumanA(const std::string& name, Weapon& weapon)
+	: name_(name), weapon_(weapon) {}
+
+void HumanA::attack() const
+{std::cout << name_ << " attacks with their " << weapon_.getType() << std::endl;}
---
+++ C01/ex03/Weapon.hpp
@@ -0,0 +1,21 @@
+#ifndef WEAPON_HPP
+ #define WEAPON_HPP
+
+#include <string>
+
+
+class Weapon
+{
+	private:
+		std::string type_;
+	
+	public:
+		Weapon(const std::string& type);
+
+		const std::string& getType() const;
+		void setType(const std::string& type);
+
+};
+
+
+#endif
