6978ef6bd2dacf61c453e30c5fb8f3cf872ae8b0
dw <doglasbastos@hotmail.com>
2026-06-04 10:09:06 +0200

    M7 Ex00 done

---
+++ C07/ex00/main.cpp
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dleite-b <dleite-b@student.42lausanne.c    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/06/04 10:08:32 by dleite-b          #+#    #+#             */
+/*   Updated: 2026/06/04 10:08:33 by dleite-b         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "whatever.hpp"
+
+#include <iostream>
+
+int main( void ) {
+    int a = 2;
+    int b = 3;
+
+    ::swap( a, b );
+
+    std::cout << "a = " << a << ", b = " << b << std::endl;
+    std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+    std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+
+    std::string c = "chaine1";
+    std::string d = "chaine2";
+
+    ::swap(c, d);
+
+    std::cout << "c = " << c << ", d = " << d << std::endl;
+    std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+    std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+
+    return 0;
+}
---
+++ C07/ex00/whatever.hpp
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   whatever.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dleite-b <dleite-b@student.42lausanne.c    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/06/04 10:08:35 by dleite-b          #+#    #+#             */
+/*   Updated: 2026/06/04 10:08:36 by dleite-b         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef WHATEVER_HPP
+#define WHATERVER_HPP
+
+template <typename T>
+void swap(T& a, T& b)
+{
+    T tmp = a;
+    a = b;
+    b = tmp;
+}
+
+template<typename T>
+const T& min(const T& a, const T& b)
+{
+    return (a < b ? a : b);
+}
+
+template <typename T>
+const T& max(const T& a, const T& b)
+{
+    return(a > b ? a : b);
+}
+
+
+#endif
